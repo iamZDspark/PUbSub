9953c6a638b8f844369360df0448c8e821dede7f
Shailesh Prajapati <shailesh.welcome@gmail.com>
2019-11-03 23:19:35 +0530

    Initial Commit

---
+++ Application/Program.cs
@@ -0,0 +1,43 @@
+﻿using Apache.NMS;
+using System;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(text)) return;
+                SendNewMessage(text);
+            }
+        }
+
+        private static void SendNewMessage(string text)
+        {
+            string topic = "TextQueue";
+
+            Console.WriteLine($"Adding message to queue topic: {topic}");
+
+            string brokerUri = $"activemq:tcp://localhost:61616";  // Default port
+            NMSConnectionFactory factory = new NMSConnectionFactory(brokerUri);
+
+            using (IConnection connection = factory.CreateConnection())
+            {
+                connection.Start();
+
+                using (ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
+                using (IDestination dest = session.GetTopic(topic))
+                using (IMessageProducer producer = session.CreateProducer(dest))
+                {
+                    producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
+
+                    producer.Send(session.CreateTextMessage(text));
+                    Console.WriteLine($"Sent {text} messages");
+                }
+            }
+        }
+    }
+}
---
+++ subscriber2/Program.cs
@@ -0,0 +1,55 @@
+﻿using Apache.NMS;
+using System;
+
+namespace subscriber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Waiting for messages");
+
+            // Read all messages off the queue
+            while (ReadNextMessage())
+            {
+                Console.WriteLine("Successfully read message");
+            }
+
+            Console.WriteLine("Finished");
+        }
+
+        static bool ReadNextMessage()
+        {
+            string topic = "TextQueue";
+
+            string brokerUri = $"activemq:tcp://localhost:61616";  // Default port
+            NMSConnectionFactory factory = new NMSConnectionFactory(brokerUri);
+
+            using (IConnection connection = factory.CreateConnection())
+            {
+                connection.Start();
+                using (ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
+                using (IDestination dest = session.GetTopic(topic))
+                using (IMessageConsumer consumer = session.CreateConsumer(dest))
+                {
+                    IMessage msg = consumer.Receive();
+                    if (msg is ITextMessage)
+                    {
+                        ITextMessage txtMsg = msg as ITextMessage;
+                        string body = txtMsg.Text;
+
+                        Console.WriteLine($"Received message: {txtMsg.Text}");
+
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Unexpected message type: " + msg.GetType().Name);
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
